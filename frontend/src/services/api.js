f238d072553182dc5c992f5e5f2abc7bf0af7951
Rishithasai1712 <rishithasai98@gmail.com>
2026-03-31 13:31:37 +0530

    Initial project setup (frontend + backend)

---
+++ frontend/src/services/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { getToken } from "../utils/token";
+
+const API = axios.create({
+  baseURL: "http://localhost:8080/api",
+});
+
+ 
+API.interceptors.request.use((req) => {
+  const token = getToken();
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+  return req;
+});
+
+export default API;
